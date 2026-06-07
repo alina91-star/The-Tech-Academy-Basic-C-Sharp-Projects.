e3cd44bd74c41d5d1e500d2b06284f3fe257a1b9
Alina <alinadiac91@gmail.com>
2026-06-07 21:57:51 +0100

    commit

---
+++ WhileDoWhileAssignment/WhileDoWhileAssignment/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace WhileDoWhileAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a boolean variable for the while loop
+            bool isTrue = true;
+
+            // Create an integer counter
+            int count = 0;
+
+            // While loop runs as long as isTrue is true
+            while (isTrue)
+            {
+                // Display the current count value
+                Console.WriteLine("While Loop Count: " + count);
+
+                // Increase count by 1
+                count++;
+
+                // When count reaches 5, change isTrue to false
+                if (count == 5)
+                {
+                    isTrue = false;
+                }
+            }
+
+            // Create a boolean variable for the do while loop
+            bool isFalse = false;
+
+            // Create another counter variable
+            int number = 0;
+
+            // Do block executes at least once
+            do
+            {
+                // Display the current number value
+                Console.WriteLine("Do While Loop Count: " + number);
+
+                // Increase number by 1
+                number++;
+
+                // When number reaches 5, change isFalse to true
+                if (number == 5)
+                {
+                    isFalse = true;
+                }
+
+            } while (!isFalse);
+
+            // Pause the console window
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WhileDoWhileAssignment/WhileDoWhileAssignment/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WhileDoWhileAssignment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WhileDoWhileAssignment")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("28c3ca70-8ba0-428e-b6a4-7e7683940904")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
