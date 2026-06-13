24b3621f5606cc97008b4afa89c0105b8d9fb17b
Alina <alinadiac91@gmail.com>
2026-06-13 15:37:08 +0100

    AbstractClassAssignment

---
+++ AbstractClassAssignment/AbstractClassAssignment/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AbstractClassAssignment
+{
+    // Employee inherits from the abstract Person class.
+    public class Employee : Person
+    {
+        // This method implements the abstract SayName method.
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ AbstractClassAssignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AbstractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate and initialize an Employee object.
+            Employee employee = new Employee()
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+            // Call the SayName method on the Employee object.
+            employee.SayName();
+
+            // Keep the console window open.
+            Console.ReadLine();
+        }
+    }
+}
